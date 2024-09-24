89abb1d5d0d5e0d01efdee9d9e1325963190a733
ArmandoBolanios <mordana_93@hotmail.com>
2024-09-24 15:52:57 -0600

    mensaje 1

---
+++ src/com/aluracursos/radioalura/principal/Principal.java
@@ -0,0 +1,26 @@
+package com.aluracursos.radioalura.principal;
+
+import com.aluracursos.radioalura.modelos.Cancion;
+import com.aluracursos.radioalura.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Cancion miCancion = new Cancion();
+        miCancion.setTitulo("Forever");
+        miCancion.setCantante("Kiss");
+        
+        Podcast miPodcast = new Podcast();
+        miPodcast.setPresentador("Hermenegildo");
+        miPodcast.setTitulo("Cafe.tech");
+        
+        for (int i = 0; i < 100; i++) {
+            miCancion.meGusta();
+        }
+        for (int i = 0; i < 500; i++) {
+            miCancion.reproduce();
+        }
+        
+        System.out.println("Total de reproducciones: "+miCancion.getTotalDeReproducciones());
+        System.out.println("Total de me gusta: "+miCancion.getTotalDeMegusta());
+    }
+}
