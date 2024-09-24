c31671d707df06556baac2b0704bc5c80d91ece6
ArmandoBolanios <mordana_93@hotmail.com>
2024-09-24 16:10:35 -0600

    Segundo mensaje

--- src/com/aluracursos/radioalura/principal/Principal.java
+++ src/com/aluracursos/radioalura/principal/Principal.java
@@ -1,6 +1,7 @@
 package com.aluracursos.radioalura.principal;
 
 import com.aluracursos.radioalura.modelos.Cancion;
+import com.aluracursos.radioalura.modelos.MisFavoritos;
 import com.aluracursos.radioalura.modelos.Podcast;
 
 public class Principal {
@@ -11,16 +12,28 @@ public class Principal {
         
         Podcast miPodcast = new Podcast();
         miPodcast.setPresentador("Hermenegildo");
-        miPodcast.setTitulo("Cafe.tech");
+        miPodcast.setTitulo("Cafe Tech");
         
+        //******CANCION
         for (int i = 0; i < 100; i++) {
             miCancion.meGusta();
         }
-        for (int i = 0; i < 500; i++) {
+        for (int i = 0; i < 2000; i++) {
             miCancion.reproduce();
         }
         
+        //******PODCAST
+        for (int i = 0; i < 100; i++) {
+            miPodcast.meGusta();
+        }
+        for (int i = 0; i < 8000; i++) {
+            miPodcast.reproduce();
+        }
         System.out.println("Total de reproducciones: "+miCancion.getTotalDeReproducciones());
         System.out.println("Total de me gusta: "+miCancion.getTotalDeMegusta());
+        
+        MisFavoritos favoritos = new MisFavoritos();
+        favoritos.incluye(miPodcast);
+        favoritos.incluye(miCancion);
     }
 }
